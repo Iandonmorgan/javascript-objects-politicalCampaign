45b3f4d85cad3ce5921af8240e8fa108fbb69a56
Landon Morgan <morganlandon@gmail.com>
2020-01-20 15:32:14 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,84 @@
+/*
+
+You have volunteered your time to a local political candidate,
+Elizabeth Sanger, who wants to become a US representative in
+Congress for your district. Unfortunately, the team discovered
+that you're a software developer, so they have begged you to build
+an application that lets them track volunteers, and store information
+about Elizabeth and her campaign.
+
+Your job is to define the different objects and arrays, their
+structure, and the corresponding properties for each, to represent
+the following information about Elizabeth's campaign.
+*/
+
+
+// Her congressional district (you can use yours here)
+
+const congressionalDistrict = "";
+
+// Her platform statements for the following issues.
+// Taxes
+// Jobs
+// Infrastructure
+// Health care
+// Crime and enforcement
+
+const politicalPlatform = {
+    taxes:"",
+    jobs:"",
+    infrastructure:"",
+    healthCare:"",
+    crime:""
+};
+
+// URL for donation form
+
+const donationLink = "";
+
+// Calendar of events
+
+const eventsCalendar = [];
+
+console.table(eventsCalendar);
+
+// Volunteer information
+    // Name
+    // Address
+    // Email
+    // Phone number
+    // Availability
+    // What activities each one is willing to do (e.g. answering phone calls, taking polls, etc.)
+
+const volunteerInfo = {
+    name:"",
+    address:"",
+    email:"",
+    phone:"",
+    avail:"",
+    activities:""
+
+};
+
+// Biography
+
+const bio = "";
+
+// Image gallery
+    // Head shot
+    // Picture of family
+    // Picture of constituents
+
+const imageGallery = {
+    headShot:"",
+    family:"",
+    constituents:""
+}
+
+// Mission statement
+
+const missionStatement = "";
+
+// URL for registering to vote
+
+const voterRegistrationURL = "";
